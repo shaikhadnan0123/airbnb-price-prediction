3b30ae4d2567bd08f7b13cbb46a3be22a3404b8b
Shaikh Adnan <your_email@example.com>
2026-08-20 10:48:19 +0530

    Connect frontend to deployed Render API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -182,13 +182,16 @@ function App() {
     setPredictedPrice(null);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
+  const response = await fetch(
+    "https://airbnb-price-prediction-lcnt.onrender.com/predict",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    }
+  );
 
       const result = await response.json();
 
